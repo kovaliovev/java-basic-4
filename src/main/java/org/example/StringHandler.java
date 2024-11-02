143b7c7a1e9481819f0b5281ca53e8447ab6d8f6
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-11-02 21:25:20 +0200

    Implement removing specific words from text

---
+++ src/main/java/org/example/StringHandler.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class StringHandler {
+
+    public static Text removeWords(Text inputText, int lengthToRemove) {
+
+        Text result = new Text("");
+
+        for (Sentence sentence : inputText.getSentences()) {
+            Sentence newSentence = new Sentence("");
+            for (Object element : sentence.getElements()) {
+                System.out.println(element);
+                if (element instanceof Word) {
+                    Word word = (Word) element;
+                    if (word.length() == lengthToRemove && word.startsWithConsonant()) {
+                        continue;
+                    }
+                }
+                newSentence.getElements().add(element);
+            }
+            result.getSentences().add(newSentence);
+        }
+        return result;
+    }
+}
