5560d01b5c92de14f025a5c84485a78ed1a71398
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-11-02 21:28:38 +0200

    Add usage example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,14 @@ package org.example;
 
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Text inputText = new Text("This is an example of text, entered by user, that needs processing.");
+
+        int wordToRemoveLength = 4;
+
+        Text result = StringHandler.removeWords(inputText, wordToRemoveLength);
+
+        System.out.println("Original text: " + inputText);
+        System.out.println("Formatted text: " + result);
+    }
 }
